eb6604e14d9bc7a45357b468530f697fa42b98bf
Gérôme Chardon <gerchardon@users.noreply.github.com>
2014-11-01 20:42:32 +0100

    Update autosub

--- test/plugin.opensub.js
+++ test/plugin.opensub.js
@@ -1,8 +1,26 @@
-var autosub = require('../lib');
+var should = require('chai').should();
+
+var opensub = require('../lib/plugins/opensub');
+var tools = require('video-tools');
+var info = tools.info('Scorpion.S01E04.HDTV.x264-LOL.mp4');
+var winston = require("winston");
 
 describe('autosub.plugins.opensub', function() {
-    it('exist', function(done) {
-        autosub().test();
-        done();
+    this.timeout(5000);
+
+    it('search length', function(done) {
+        opensub.search(info).then(function(results) {
+            should.exist(results);
+            results.should.be.a('array');
+            results.should.not.empty;
+
+            results.forEach(function(r){
+                winston.debug("zzzz");
+                console.log(r.MovieKind);
+                console.log(r.SubLanguageID);
+                console.log(r.SubFormat);
+            });
+            done();
+        }).done();
     });
 });
